40b55ff2fced352a5d1ea9f2ec2c74532df670c6
Huda-Mawood <huda.mawod881@compit.aun.edu.eg>
2024-12-09 22:20:38 +0200

    add project animation

---
+++ animation-master/Assets/Scenes/NumberChanger.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberChanger : MonoBehaviour
+{
+    public Image[] images; // مجموعة الصور
+    private int index = 0; // الفهرس الحالي للصورة
+
+    void Start()
+    {
+        // تأكد من أن هناك صور في المصفوفة
+        if (images.Length == 0)
+        {
+            Debug.LogError("Images array is empty!");
+            return; // Exit early to avoid errors
+        }
+
+        // تعيين جميع الصور كغير مرئية عند بداية اللعبة
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].gameObject.SetActive(false);
+        }
+
+        // تأكد من أن الفهرس يبدأ من 0 أو الصورة الأولى فقط إذا لزم الأمر
+        Debug.Log("Initial Image State: All images are hidden.");
+    }
+
+    public void NextImage()
+    {
+        // تأكد من أن هناك صور في المصفوفة
+        if (images.Length == 0)
+        {
+            Debug.LogError("Images array is empty!");
+            return; // Exit early to avoid errors
+        }
+
+        // تعيين جميع الصور كغير مرئية
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].gameObject.SetActive(false);
+        }
+
+        // عرض الصورة التالية
+        index = (index + 1) % images.Length; // التنقل بين الصور
+        images[index].gameObject.SetActive(true);
+
+        Debug.Log("Current Image Index: " + index);
+    }
+
+    public void PreviousImage()
+    {
+        // تأكد من أن هناك صور في المصفوفة
+        if (images.Length == 0)
+        {
+            Debug.LogError("Images array is empty!");
+            return; // Exit early to avoid errors
+        }
+
+        // تعيين جميع الصور كغير مرئية
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].gameObject.SetActive(false);
+        }
+
+        // عرض الصورة السابقة
+        index = (index - 1 + images.Length) % images.Length; // التنقل بين الصور
+        images[index].gameObject.SetActive(true);
+
+        Debug.Log("Current Image Index: " + index);
+    }
+}
---
+++ animation-master/Assets/Scenes/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // تأكد من أن هذه الدالة public
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main menu");
+    }
+    public void Alphapitic()
+    {
+        SceneManager.LoadScene("Alphapitic");
+    }
+    public void Numbers()
+    {
+        SceneManager.LoadScene("Numbers");
+    }
+    public void QuizNumbers()
+    {
+        SceneManager.LoadScene("QuizNumbers");
+    }
+    public void QuizAlphabetic()
+    {
+        SceneManager.LoadScene("QuizAlphabetic");
+    }
+    public void Quiz()
+    {
+        SceneManager.LoadScene("Quiz");
+    }
+
+
+}
